821ed4e055906921759ecdf8a247fd8bcb174a24
mmudimbu <mmudimbu@hitrac.co.zw>
2021-03-09 16:29:11 +0200

    added button and function to get solution and update state, changed endpoint in handle new to match the one in my backend for new puzzle

--- src/index.js
+++ src/index.js
@@ -20,8 +20,9 @@ class Board extends React.Component {
         this.state = {
             squares: Array(9).fill(null),
             blank: null,
-        };        
-        
+            solution: null
+        };
+
         this.IsSolutionPresent = () => {
             var solution = this.state.solution
             if(solution == null){
@@ -56,7 +57,7 @@ class Board extends React.Component {
         const blank = this.state.blank;
         const diff = i - blank;
 
-        if ([1, 3].includes(Math.abs(diff))) {
+        if ( [1, 3].includes(Math.abs(diff))) {
             const squares = this.state.squares.slice();
             squares[i] = 0;
             squares[this.state.blank] = this.state.squares[i]
@@ -67,27 +68,22 @@ class Board extends React.Component {
     }
 
     handleNew() {
-        fetch('/initial').then(res => res.json()).then(data => {
+        fetch('/startState').then(res => res.json()).then(data => {
             this.setState({
                 squares: data,
-                blank: data.indexOf(0),
-                solution: null
+                blank: data.indexOf(0)
             });
         });
     }
 
-    handleSolve() {
-        fetch('/solution', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.state.squares)
-        }).then(res => res.json()).then(data => {
+    getSolution() {
+
+        fetch('/solution?initial_state=' + (this.state.squares).toString()).then(res => res.json()).then(data => {
             this.setState({
-                solution: data,
-                blank: data.indexOf(0)
+                solution: data
             });
+            console.log(data)
+            alert('Done')
         });
     }
 
@@ -101,11 +97,10 @@ class Board extends React.Component {
     }
 
     render() {
-
+       
         return (
             <div>
                 <button className='tool' onClick={() => this.handleNew()}>New</button>
-                <button className='tool' onClick={() => this.handleSolve()}>Solve</button>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -121,17 +116,10 @@ class Board extends React.Component {
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </div>
-                {this.state.solution && (
-                    <div className="game-info">
-                        <div><h3>Action Sequence:</h3></div>
-                        <ol>{this.state.solution.map((action, index) => (
-                            <li key={index}>{action}</li>
-                        ))
-                        }</ol>
-                    </div>
-                )}
+                <button className='tool' onClick={() => this.getSolution()}>Get Solution</button>
+                <br/>
+                <this.IsSolutionPresent/>
             </div>
-
         );
     }
 }
@@ -143,7 +131,10 @@ class Game extends React.Component {
                 <div className="game-board">
                     <Board />
                 </div>
-                
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
             </div>
         );
     }
